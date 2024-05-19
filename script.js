27f6860460df7dd16032dff5d3061d04abc2f485
Kainat zia <154695099+Kainat-zia@users.noreply.github.com>
2024-05-19 21:31:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// assignment 1
+
+alert("Error! Please enter a valid password");
+
+alert("Welcome to JS Land...\nHappy Coding");
+
+alert("Welcome to JS Land...");
+
+console.log("Hellow...i can run JS through my web broswers console");
+alert("Hellow...i can run JS through my web broswers console")
+
+// assignment 2
+
+var username
+var myName ="Kainat zia";
+
+var message ="Hellow World";
+alert("Message:"+ "  " + message);
+
+var name = "Jhone Doe";
+alert(name);
+var age = "15 years old";
+alert(age);
+var course = "Certified Mobile Application Development";
+alert(course);
+
+var pizza = "PIZZA";
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+var email = "kainatzia4663@gmail.com";
+alert("My email address is" + " " + email )
+
+var book = "A smarter way to learn Javascript";
+alert("I am trying to learn  from the book" + " " + book)
+
+document.write("Yah! I can write HTML content through Javascript")
+
+var srting = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(string);
